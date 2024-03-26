03293608c0bdc0f3b62c0c10e6565a1e48ad3208
Heavy108 <Heavy1089@gmail.com>
2024-03-26 22:17:24 +0530

    first commit

---
+++ create_react_comp/create_react_comp/src/App.jsx
@@ -0,0 +1,10 @@
+import KgButton from "./comp";
+function  App(){
+  return <div>
+    <h1>Am I Alive?</h1>
+    <KgButton></KgButton>
+  </div>
+  
+}
+
+export default App;
---
+++ create_react_comp/create_react_comp/src/comp.jsx
@@ -0,0 +1,5 @@
+function KgButton(){
+  return <button>click me </button>
+}
+
+export default KgButton;
