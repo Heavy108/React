dbc09b69e8ca4ebc34a2b195fda06ace147bb46c
Heavy108 <Heavy1089@gmail.com>
2024-03-26 23:35:09 +0530

    React_component2

--- create_react_comp/create_react_comp/src/App.jsx
+++ create_react_comp/create_react_comp/src/App.jsx
@@ -1,7 +1,9 @@
 import KgButton from "./comp";
+import Hello from "./Hello";
 function  App(){
   return <div>
     <h1>Am I Alive?</h1>
+    <Hello></Hello>
     <KgButton></KgButton>
   </div>
   
---
+++ create_react_comp/create_react_comp/src/Hello.jsx
@@ -0,0 +1,11 @@
+function Hello() {
+  let name = "Satyam Sajal";
+
+  let full_name = () => {
+    return "Anthony Gonsalvis";
+  }
+
+  return <h4>Hello this is The Future  and  {full_name()}</h4>;
+}
+
+export default Hello;
