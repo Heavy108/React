59fc08b7bbf8970e6ffa12f0dce2db56221430cb
Heavy108 <Heavy1089@gmail.com>
2024-03-30 04:24:34 +0530

    version2

---
+++ Projects/TODO_List_Version2/src/profile.jsx
@@ -0,0 +1,9 @@
+// function Profile(){
+//   return (
+//     <img src="https://i.imgur.com/MK3eW3As.jpg" alt="Katherine 
+//     Johnson"></img>
+//     <img src="https://i.imgur.com/QIrZWGIs.jpg"></img>
+//   );
+// }
+
+// export default Profile;
