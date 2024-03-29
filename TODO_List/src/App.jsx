a7c15cb93aae5817af8887e50f06564d7e944f7d
Heavy108 <Heavy1089@gmail.com>
2024-03-29 21:04:38 +0530

    Todos

--- TODO_List/src/App.jsx
+++ TODO_List/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import Profile from "./profile";
 
-function App() {
-  const [count, setCount] = useState(0)
+// function Gallery(){
+//   return (
+//     <section>
+//       <h1>
+//         Amazing Scientists
+//       </h1>
+//       <Profile />
+//       <Profile />
+//     </section>
+//   );
+// }
 
+// export default  Gallery;
+
+export default function TodoList(){
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>Heavy108 Todos</h1>
+    <img
+    src="https://i.imgur.com/yXOvdOSs.jpg"
+    alt="Hedy Lamarr"
+    class="photo"></img>
+     <ul>
+      <li>Invent new traffic lights</li>
+      <li>Rehearse a movie scene</li>
+      <li>Improve spectrum technology</li>
+    </ul>
     </>
-  )
+  );
 }
-
-export default App
